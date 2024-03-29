14031581d87569af6f2ea0f71afed9dff32f3008
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-03-29 10:03:41 +0530

    Update README and add currency converter functionality

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-  const [data, setData] = useState({});
+    const [data, setData] = useState({});
 
-  useEffect(() => {
-    fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-    )
-      .then((res) => res.json())
-      .then((res) => setData(res[currency]));
-    console.loG(data);
-  }, [currency]);
-  console.log(data);
-  return data;
+    useEffect(() => {
+        fetch(
+            `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+        )
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]));
+        console.log(data);
+    }, [currency, data]); // Include 'data' in the dependency array
+    console.log(data);
+    return data;
 }
 
 export default useCurrencyInfo;
